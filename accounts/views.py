492a22913f57dbeb037cc4164aa2303d90504ed5
Vicky Dalmia <vicky8394@gmail.com>
2018-06-14 12:35:48 +0530

    Login Logout Functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
-# Create your views here.
+def accounts(request):
+    return render(request, 'home.html')
+
+
+
+def signup(request):
+    if request.method == 'POST':
+        #do something
+        if request.POST['userpassword'] == request.POST['confirmuserpassword']:
+            try:
+                u = User.objects.get(username=request.POST['username'])
+                return render(request, 'accounts/signup.html', {'error': 'Username already has been taken'})
+            except User.DoesNotExist:
+                u = User.objects.create_user(request.POST['username'], password=request.POST['userpassword'])
+                auth.login(request, u)
+                return redirect('home')
+        else:
+            return render(request, 'accounts/signup.html', {'error': 'Password Validaton Failed'})
+    else:
+        return render(request, 'accounts/signup.html')
+
+
+
+def login(request):
+    if request.method == 'POST':
+        user = auth.authenticate(username=request.POST['username'], password=request.POST['userpassword'])
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'accounts/login.html', {'error':'Username or password is incorrect or doesn\'t exist'})
+    else:
+        return render(request, 'accounts/login.html')
+
+
+
+def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('home')
--- products/models.py
+++ products/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Product(models.Model):
+    image = models.ImageField(upload_to='images/')
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField()
+    votes_total = models.IntegerField(default=1)
+    icon = models.ImageField(upload_to='images/')
+    body = models.TextField()
+    url = models.TextField()
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def pretty_date(self):
+        return self.pub_date.strftime('%b %e %Y')
+
+    def summary(self):
+        return self.body[:100]
